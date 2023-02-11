c42c27fc7888ebf7ebb00a09334e4d0161a4dfc9
Luke Lawlor <lklawlor1@gmail.com>
2023-02-10 19:22:14 -0500

    added pong ball and color macros

--- games/pong/src/main.c
+++ games/pong/src/main.c
@@ -9,16 +9,36 @@
 #define	WIN_WIDTH	800
 #define	WIN_HEIGHT	800
 
+// Object colors
+#define	PADDLE_COLOR_R	255
+#define	PADDLE_COLOR_G	0
+#define	PADDLE_COLOR_B	0
+
+#define	BALL_COLOR_R	0
+#define	BALL_COLOR_G	0
+#define	BALL_COLOR_B	255
+
+#define	WIN_COLOR_R	0
+#define	WIN_COLOR_G	0
+#define	WIN_COLOR_B	0
+
+// Border around the game world
 #define	ARENA_BORDER	10
 
 // Paddle settings
 #define	PADDLE_SPEED	16
-
 #define	PADDLE_WIDTH	12
 #define	PADDLE_HEIGHT	40
-
 #define	PADDLE_Y_MIN	ARENA_BORDER
 #define	PADDLE_Y_MAX	(WIN_HEIGHT - ARENA_BORDER - PADDLE_HEIGHT)
+
+// Ball settings
+#define	BALL_WIDTH	20
+#define	BALL_HEIGHT	20
+#define	BALL_X_MIN	ARENA_BORDER
+#define	BALL_X_MAX	(WIN_WIDTH - ARENA_BORDER - BALL_WIDTH)
+#define	BALL_Y_MIN	ARENA_BORDER
+#define	BALL_Y_MAX	(WIN_HEIGHT - ARENA_BORDER - BALL_HEIGHT)
 
 // Keys
 #define	KEY_PADDLE_LEFT_UP	SDL_SCANCODE_W
@@ -38,19 +58,23 @@ typedef struct Paddle{
 	double y;
 } Paddle;
 
+typedef struct Ball{
+	double x;
+	double y;
+	double xs;
+	double ys;
+} Ball;
+
 double clamp(double value, int min, int max);
 
-void paddle_draw(Paddle *paddle)
-{
-	SDL_SetRenderDrawColor(g_ren, 0, 0, 0, 255);
-	SDL_Rect drect = {
-		(int) paddle->x,
-		(int) paddle->y,
-		PADDLE_WIDTH,
-		PADDLE_HEIGHT,
-	};
-	SDL_RenderFillRect(g_ren, &drect);
-}
+// Draws a paddle
+void paddle_draw(Paddle *paddle);
+
+// Updates a ball
+void ball_update(Ball *ball);
+
+// Draws a ball
+void ball_draw(Ball *ball);
 
 int main(int argc, char **argv)
 {
@@ -94,6 +118,7 @@ int main(int argc, char **argv)
 	// Get the keyboard state
 	const Uint8 *key_state = SDL_GetKeyboardState(NULL);
 
+	// Initialize game objects
 	Paddle left_paddle = {
 		ARENA_BORDER,
 		ARENA_BORDER,
@@ -101,6 +126,12 @@ int main(int argc, char **argv)
 	Paddle right_paddle = {
 		WIN_WIDTH - ARENA_BORDER - PADDLE_WIDTH,
 		ARENA_BORDER,
+	};
+	Ball ball = {
+		80,
+		80,
+		4,
+		4,
 	};
 
 	// Game loop
@@ -135,7 +166,6 @@ int main(int argc, char **argv)
 			right_paddle.y -= PADDLE_SPEED;
 		if (key_state[KEY_PADDLE_RIGHT_DOWN])
 			right_paddle.y += PADDLE_SPEED;
-
 		left_paddle.y = clamp(
 			left_paddle.y,
 			PADDLE_Y_MIN,
@@ -146,6 +176,7 @@ int main(int argc, char **argv)
 			PADDLE_Y_MIN,
 			PADDLE_Y_MAX
 		);
+		ball_update(&ball);
 
 		//if (key_state[KEY_PADDLE_RIGHT_UP])
 		//	left_paddle.y += PADDLE_SPEED;
@@ -153,11 +184,18 @@ int main(int argc, char **argv)
 //		if (key_state[KEY_PADDLE_RIGHT_DOWN])
 //			left_paddle.y -= PADDLE_SPEED;
 
-		SDL_SetRenderDrawColor(g_ren, 255, 200, 100, 255);
+		SDL_SetRenderDrawColor(
+			g_ren,
+			WIN_COLOR_R,
+			WIN_COLOR_G,
+			WIN_COLOR_B,
+			255
+		);
 		SDL_RenderClear(g_ren);
 
 		paddle_draw(&left_paddle);
 		paddle_draw(&right_paddle);
+		ball_draw(&ball);
 
 		SDL_RenderPresent(g_ren);
 	}
@@ -177,4 +215,64 @@ double clamp(double value, int min, int max)
 	if (value > max)
 		return max;
 	return value;
+}
+
+// Draws a paddle
+void paddle_draw(Paddle *paddle)
+{
+	SDL_SetRenderDrawColor(
+		g_ren,
+		PADDLE_COLOR_R,
+		PADDLE_COLOR_G,
+		PADDLE_COLOR_B,
+		255
+	);
+	SDL_Rect drect = {
+		(int) paddle->x,
+		(int) paddle->y,
+		PADDLE_WIDTH,
+		PADDLE_HEIGHT,
+	};
+	SDL_RenderFillRect(g_ren, &drect);
+}
+
+// Updates a ball
+void ball_update(Ball *ball)
+{
+	ball->x += ball->xs;
+	if (ball->x > BALL_X_MAX)
+	{
+		ball->x = BALL_X_MAX;
+		ball->xs *= -1.1;
+	}
+	else if (ball->x < BALL_X_MIN)
+	{
+		ball->x = BALL_X_MIN;
+		ball->xs *= -1.2;
+	}
+	ball->y += ball->ys;
+	if (ball->y > BALL_Y_MAX)
+	{
+		ball->y = BALL_Y_MAX;
+		ball->ys *= -1.3;
+	}
+	else if (ball->y < BALL_Y_MIN)
+	{
+		ball->y = BALL_Y_MIN;
+		ball->ys *= -1.4;
+	}
+}
+
+// Draws a ball
+void ball_draw(Ball *ball)
+{
+	SDL_SetRenderDrawColor(
+		g_ren,
+		BALL_COLOR_R,
+		BALL_COLOR_G,
+		BALL_COLOR_B,
+		255
+	);
+	SDL_Rect drect = {ball->x, ball->y, 20, 20};
+	SDL_RenderFillRect(g_ren, &drect);
 }
